fbe1cde9aad2f149c0d4af0b597c7159d09116b7
长沙修能-黄迪迪 <1277645860@qq.com>
2022-09-05 11:34:21 +0800

    配置自己的测试接口 修改了生日等参数设置

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ export const config = {
    */
 
   // 公众号APP_ID
-  APP_ID: "",
+  APP_ID: "wx63500ae58976720f",
 
   // 公众号APP_SECRET
-  APP_SECRET: "",
+  APP_SECRET: "0d8a9b2952e37237f8a36b365d3f5251",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
@@ -21,32 +21,32 @@ export const config = {
    *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
-   *   }, 
+   *   },
    * ]
    */
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "迪迪",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "oNcJj6KtwS063ae6R0jgUzwoNh2o",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "FFZBAPO6eqQA2GdArRQ0Zv1BI3AsmwL07NPijLtcbYY",
       // 所在省份
-      province: "广东",
+      province: "湖南",
       // 所在城市
-      city: "肇庆",
+      city: "长沙",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "老婆1", 
+      openUrl: "http://47.112.119.73:8084/"
+    },
+    /*{
+      name: "老婆1",
       id: "",
       useTemplateId: "",
       province: "",
       city: "",
       openUrl: "https://wangxinleo.cn"
-    }, 
+    },
     {
       name: "老婆2",
       id: "",
@@ -54,15 +54,15 @@ export const config = {
       province: "",
       city: "",
       openUrl: "https://wangxinleo.cn"
-    }, 
+    },
     {
-      name: "老婆3", 
+      name: "老婆3",
       id: "",
       useTemplateId: "",
       province: "",
       city: "",
       openUrl: "https://wangxinleo.cn"
-    }, 
+    },*/
   ],
 
   /**
@@ -70,7 +70,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+  CALLBACK_TEMPLATE_ID: "L3sPPEK0gyPVDoGcJmkitOYXyii2PPPnpPDE8wCotFY",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -78,10 +78,10 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
-    }, 
+      id: "oNcJj6KtwS063ae6R0jgUzwoNh2o",
+    },
   ],
-    
+
   /**
    * 信息配置
    */
@@ -89,9 +89,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "湖南",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "长沙",
 
   /** 重要节日相关 */
 
@@ -102,42 +102,42 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
+    {"type": "生日", "name": "迪迪", "year": "1999", "date": "03-07"},/*
     {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
     {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},*/
   ],
 
   /**
-   * 限制重要节日的展示条目, 需要填写数字; 
+   * 限制重要节日的展示条目, 需要填写数字;
    * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
    * 如果为0, 则默认展示全部
    */
-  FESTIVALS_LIMIT: 4,
+  FESTIVALS_LIMIT: 0,
 
   /** 日期相关 */
 
   /** 你现在可以随心增加你认为的所有的需要纪念的日子啦！
     * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-    * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }} 
+    * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }}
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
+/*    {"keyword": "love_day", date: "2015-05-01"},
     // 结婚纪念日
-    {"keyword": "marry_day", date: "2020-01-04"},
+    {"keyword": "marry_day", date: "2020-01-04"},*/
     // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2022-08-31"},
+    {"keyword": "ex_day", date: "2018-09-01"},
     // sakana日
     // {"keyword": "sakana_day", date: "2022-01-06"},
-    // ... 
+    // ...
   ],
 
   /** 插槽 */
 
   /** 你可以在这里写超多的你想显示的内容了！
     * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
-    * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }} 
+    * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
     * */
   SLOT_LIST: [
     // 这样配置的话，就会每次发送这句话
